093768c203fd159c71463e68e3ab2f9600920426
Entreprenerd <alex@entreprenerd.xyz>
2023-03-17 14:19:35 +0100

    feat: profit logic for stat arber

--- classes/users/stat_arber.py
+++ classes/users/stat_arber.py
@@ -5,18 +5,7 @@ from classes.users.borrower import Borrower
 
 
 ## Borrow and Sells when price is higher
-class StatArber(Borrower):
-    def __init__(self, system, initial_balance_collateral):
-        Borrower.__init__(self, system, initial_balance_collateral)
-
-        ## TODO
-        self.position = 0
-        self.target_open_price = 0
-        self.target_profit_percent = 0
-        self.target_loss_percent = 0
-
-
-
+"""
     ## Borrow and Sell
 
     ## If profit is above target close
@@ -27,13 +16,29 @@ class StatArber(Borrower):
 
     ## Patience value
 
-    ## Price Open
+    ## Price Open
     ## Profit target
     ## Loss target
 
     ## Lifetime PNL
     ## Total Loss
     ## Total Gain
+"""
+class StatArber(Borrower):
+    def __init__(self, system, initial_balance_collateral):
+        Borrower.__init__(self, system, initial_balance_collateral)
+
+        ## Track open position
+        self.position_sold_debt = 0
+        self.position_bought_coll = 0
+
+        ## TODO: Random price generation
+        self.target_open_price = 0
+        self.target_profit_price = 0
+        self.target_loss_price = 0
+
+        self.total_loss = 0
+        self.total_gain = 0
 
     
     def take_action(self, turn, troves, pool):
@@ -49,7 +54,7 @@ class StatArber(Borrower):
             assert False
 
         ## Has open position?
-        if (self.position > 0):
+        if (self.has_position() > 0):
             self.manage_position(trove)
         else:
             self.open_position(trove)
@@ -64,14 +69,74 @@ class StatArber(Borrower):
             ## Borrow to tollerance
             self.borrow_til_target_ltv(trove, self.target_ltv)
 
+            initial_coll = self.collateral
+
+            self.position_sold_debt += self.debt
+
             ## Sell to Pool
+            ## True = We sell debt
+            self.system.pool.swap(self, True, self.debt)
 
             ## Then set state
+            after_coll = self.collateral
+            
+            ## Coll we want to sell to get back debt to repay
+            self.position_bought_coll = after_coll - initial_coll
         
     def manage_position(self, trove):
-        print("TODO")
+        price = self.system.get_price()
+        next_price = self.system.get_next_price()
 
         ## If next_price will hit the stop loss, sell now at loss
+        if next_price > self.target_loss_price:
+            ## Let's dump at a loss
+    
+            initial_debt = self.debt
+            
+            self.system.pool.swap(self, False, self.position_bought_coll)
+            
+            ## Reset that position tracking
+            self.position_bought_coll = 0
+            
+            ## Compute roi
+            ## TODO: test to ensure values are pointed to, else this is way more complex
+            after_debt = self.debt
+            delta_debt = after_debt - initial_debt
 
-        ## if price hits the gain, take profit
+            ## Some of the debt will remain as price moved against
+            self.position_sold_debt -= delta_debt
+
+            ## TODO: Compute price as coll
+            ## TODO: Standardize pricing via a function
+            loss = delta_debt * price
+            self.total_loss += loss
+
+        elif price > self.target_profit_price:
+            ## if price hits the gain, take profit
+
+            initial_debt = self.debt
+
+            self.sell_all_coll()
+
+            after_debt = self.debt
+            delta_debt = after_debt - initial_debt
+
+            cached_initial_debt = self.position_sold_debt
+
+            ## We sold everything by definition
+            assert delta_debt > self.position_sold_debt
+
+            self.position_sold_debt = 0
+
+            ## TODO: Price standardization
+            gain = (delta_debt - cached_initial_debt) * price
+            self.total_gain += gain
+
+    def sell_all_coll(self):
+        self.system.pool.swap(self, False, self.position_bought_coll)
+        self.position_bought_coll = 0
+
+    def has_position(self):
+        return self.position_bought_coll > 0
+
         

02d3efd3c81275077b53efafdf3b34838b7db65e
Petrovska <petrovska@badger.com>
2023-03-24 02:24:04 +0800

    feat: add extra assert checks for sanity and resolve redeemer issues

--- classes/users/redeemer.py
+++ classes/users/redeemer.py
@@ -1,3 +1,5 @@
+from pytest import approx
+
 from classes.users.user import User
 
 """
@@ -48,54 +50,54 @@ class RedeemArber(User):
 
             # Ensure price spot is higher for one unit of collateral, otherwise will
             # not be profitable when consider swap fees and collateral redemp fee
-            if spot_price > price:
-                return "TODO: No point in arbing"
-
-            print(
-                f"[REDEEMER]Found arb!. System price: {spot_price} and Pool Spot price: {spot_price}"
-            )
-
-            prev_coll = self.collateral
-
-            max_coll_in = pool.get_max_coll_before_next_price_sqrt(next_price)
-
-            # Cap if too much
-            to_purchase = min(prev_coll, max_coll_in)
-
-            # Value used to cap how much debt we get, never should be more that the total system
-            total_system_debt = self.system.total_debt
-            debt_receive = pool.get_price(to_purchase, pool.reserve_x, pool.reserve_y)
-
-            if debt_receive > total_system_debt:
-                to_purchase = pool.get_price(
-                    total_system_debt, pool.reserve_y, pool.reserve_x
+            premium = 1.013  # at least should be ~1.3% in the arb gap
+            if spot_price > price * premium:
+                print(
+                    f"[REDEEMER]Found arb!. System price: {price} and Pool Spot price: {spot_price}"
                 )
 
-            # Swap collateral in the pool for debt
-            debt_out = pool.swap_for_debt(to_purchase)
+                prev_coll = self.collateral
 
-            print(f"[REDEEMER]Swapped {to_purchase} in coll for {debt_out} in debt")
+                max_coll_in = pool.get_max_coll_before_next_price_sqrt(next_price)
 
-            # User Update
-            self.collateral -= to_purchase
-            self.debt += debt_out
+                # Cap if too much
+                to_purchase = min(prev_coll, max_coll_in)
 
-            # Redeem Troves
-            redeemed_coll = 0
-            for trove in troves:
-                debt_to_redeem = min(trove.debt, self.debt)
-                if debt_to_redeem > 0:
-                    redeemed_coll += trove.redeem(debt_to_redeem, self)
-                else:
-                    continue
+                # Value used to cap how much debt we get, never should be more that the total system
+                total_system_debt = self.system.total_debt
+                debt_receive = pool.get_price(
+                    to_purchase, pool.reserve_x, pool.reserve_y
+                )
 
-            # After arb we should end-up with zero debt
-            assert self.debt == 0
+                if debt_receive > total_system_debt:
+                    to_purchase = pool.get_price(
+                        total_system_debt, pool.reserve_y, pool.reserve_x
+                    )
 
-            # Final Collateral is greater than initial
-            print("self.collateral", self.collateral)
-            print("prev_coll", prev_coll)
-            ## TODO: Sometimes unprofitable
-            # assert self.collateral >= prev_coll
-            # Final Collateral is equal to initial + total collateral redeemed
-            # assert self.collateral == prev_coll + redeemed_coll
+                # Swap collateral in the pool for debt
+                debt_out = pool.swap_for_debt(to_purchase)
+
+                print(f"[REDEEMER]Swapped {to_purchase} in coll for {debt_out} in debt")
+
+                # User Update
+                self.collateral -= to_purchase
+                self.debt += debt_out
+
+                # Redeem Troves
+                redeemed_coll = 0
+                for trove in troves:
+                    debt_to_redeem = min(trove.debt, self.debt)
+                    if debt_to_redeem > 0:
+                        redeemed_coll += trove.redeem(debt_to_redeem, self)
+                    else:
+                        continue
+
+                # After arb we should end-up with zero debt
+                assert self.debt == 0
+
+                # Final Collateral is greater than initial
+                assert self.collateral >= prev_coll
+                # Final Collateral is equal to initial + total collateral redeemed
+                assert (
+                    approx(self.collateral) == prev_coll + redeemed_coll - to_purchase
+                )
